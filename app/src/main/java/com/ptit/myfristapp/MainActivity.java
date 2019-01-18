798a34430f8604e0a9ccf2a68ec89c06266349ab
vuleptit <vule268vp@gmail.com>
2019-01-19 04:29:52 +0700

    Initial commit

---
+++ app/src/main/java/com/ptit/myfristapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ptit.myfristapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
